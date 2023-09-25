7ea74dfde828859f5961d92d5d04ff5ce5b7ef10
Joao Astolfi <jpastolfi@gmail.com>
2023-09-25 22:59:01 +0100

    Criação de Dockerfiles, migration e model de Teams

---
+++ app/backend/src/database/migrations/01-teams.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import { Model, QueryInterface, DataTypes } from "sequelize";
+import ITeam from "../../Interfaces/ITeam";
+
+module.exports = {
+  up: async (queryInterface: QueryInterface) => {
+    return queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'team_name'
+      }
+    })
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    return queryInterface.dropTable('teams');
+  }
+};
---
+++ app/backend/src/database/models/SequelizeTeams.ts
@@ -0,0 +1,27 @@
+import {
+  DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional,
+} from 'sequelize';
+
+import db from '.';
+
+export default class SequelizeTeams extends
+  Model<InferAttributes<SequelizeTeams>, InferCreationAttributes<SequelizeTeams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
